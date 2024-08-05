fbca6b8a356f7da6143cb4e4ba3dcb763e098b2e
Henrique Oliveira <hrq1408@gmail.com>
2024-08-05 11:54:26 -0300

    Front: ajuste codigo e responsividade

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import  React from 'react';
-import Menu from './components/Menu/Menu';
+import Menu from './components/menu';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import './App.css';
+import './App.scss';
 import './styles/styles.scss';
 
 function App() {
@@ -11,22 +11,22 @@ function App() {
         <section class="grid grid-template-2">
           <div class="item logo">logo</div>
           <div class="item content">
-            <div class="item">Content 1</div>
-            <div class="item">Content 2</div>
-            <div class="item">Content 3</div>
-            <div class="item">Content 4</div>
+            <div class="item cont">Content 1</div>
+            <div class="item cont">Content 2</div>
+            <div class="item cont">Content 3</div>
+            <div class="item cont">Content 4</div>
           </div>
           <div class="item sidenav">
             <Menu />
           </div>
           <div class="item advert">
-            <div class="item">advert 1</div>
-            <div class="item">advert 2</div>
-            <div class="item">advert 3</div>
+            <div class="item adve">advert 1</div>
+            <div class="item adve">advert 2</div>
+            <div class="item adve">advert 3</div>
           </div>
           <div class="item project">
-            <div class="item">project 1</div>
-            <div class="item">project 2</div>
+            <div class="item proj">project 1</div>
+            <div class="item proj">project 2</div>
           </div>
           <div class="item footer">footer</div>
         </section>
